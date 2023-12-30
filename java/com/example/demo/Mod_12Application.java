43d3ddb480df1cfaf78e79ff19d97b4805712b34
Artem1548 <140644935+Artem1548@users.noreply.github.com>
2023-12-30 18:55:08 +0200

    Add files via upload

---
+++ java/com/example/demo/Mod_12Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import com.example.demo.entity.Note;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class Mod_12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mod_12Application.class, args);
+    }
+
+}
